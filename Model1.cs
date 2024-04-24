8fea9b54e8ab56f12b4d0675bedd6b476ce8d53d
nqvinh2004 <vn449367@gmail.com>
2024-04-24 21:33:33 +0700

    codefirst

---
+++ Model1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace codeF
+{
+    public class NhanVien
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Ten { get; set; }
+
+        public int Tuoi { get; set; }
+
+        [StringLength(10)]
+        public string GioiTinh { get; set; }
+
+        public int PhongBanId { get; set; }
+
+        [ForeignKey("PhongBanId")]
+        public PhongBan PhongBan { get; set; }
+    }
+
+    public class PhongBan
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Ten { get; set; }
+
+        public int CongtyId { get; set; }
+
+        [ForeignKey("CongtyId")]
+        public Congty Congty { get; set; }
+
+        public ICollection<NhanVien> NhanViens { get; set; }
+    }
+
+    public class Congty
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Ten { get; set; }
+
+        public ICollection<PhongBan> PhongBans { get; set; }
+    }
+
+    public partial class Model1 : DbContext
+    {
+        public DbSet<NhanVien> NhanViens { get; set; }
+        public DbSet<PhongBan> PhongBans { get; set; }
+        public DbSet<Congty> Congtys { get; set; }
+
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Thiết lập các quan hệ và cấu hình mô hình ở đây
+            modelBuilder.Entity<NhanVien>()
+                .HasKey(nv => nv.Id);
+            modelBuilder.Entity<PhongBan>()
+                .HasKey(pb => pb.Id);
+            modelBuilder.Entity<Congty>()
+                .HasKey(ct => ct.Id);
+
+            // Thiết lập quan hệ giữa các bảng
+            modelBuilder.Entity<NhanVien>()
+                .HasRequired(nv => nv.PhongBan)
+                .WithMany(pb => pb.NhanViens)
+                .HasForeignKey(nv => nv.PhongBanId);
+
+            modelBuilder.Entity<PhongBan>()
+                .HasRequired(pb => pb.Congty)
+                .WithMany(ct => ct.PhongBans)
+                .HasForeignKey(pb => pb.CongtyId);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace codeF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new Model1())
+            {
+                // Thêm dữ liệu vào bảng Congty
+                var congTy = new Congty { Ten = "Công ty A" };
+                db.Congtys.Add(congTy);
+                db.SaveChanges();
+
+                // Thêm dữ liệu vào bảng PhongBan
+                var phongBan = new PhongBan { Ten = "Marketing", CongtyId = congTy.Id };
+                db.PhongBans.Add(phongBan);
+                db.SaveChanges();
+
+                // Thêm dữ liệu vào bảng NhanVien
+                var nhanVien = new NhanVien { Ten = "Nguyen Van A", Tuoi = 35, GioiTinh = "Nam", PhongBanId = phongBan.Id };
+                db.NhanViens.Add(nhanVien);
+                db.SaveChanges();
+
+                // Truy vấn và hiển thị thông tin
+                var nhanViens = (from nv in db.NhanViens
+                                 join pb in db.PhongBans on nv.PhongBanId equals pb.Id
+                                 join ct in db.Congtys on pb.CongtyId equals ct.Id
+                                 where nv.GioiTinh == "Nam" &&
+                                       pb.Ten == "Marketing" &&
+                                       nv.Tuoi >= 30 && nv.Tuoi <= 40
+                                 select new
+                                 {
+                                     TenNhanVien = nv.Ten,
+                                     Tuoi = nv.Tuoi,
+                                     TenPhongBan = pb.Ten,
+                                     TenCongTy = ct.Ten
+                                 }).ToList();
+
+                Console.WriteLine("Danh sách nhân viên nam thuộc phòng Marketing và có tuổi từ 30 đến 40:");
+                foreach (var nv in nhanViens)
+                {
+                    Console.WriteLine($"Tên: {nv.TenNhanVien}, Tuổi: {nv.Tuoi}, Phòng ban: {nv.TenPhongBan}, Công ty: {nv.TenCongTy}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
